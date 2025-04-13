3e11bfb97888ae586a579e391924f70d1c67678f
jayzou <chrischau666@gmail.com>
2025-04-14 00:49:44 +0800

    build test

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,28 +1,26 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { MDXProvider } from '@mdx-js/react';
-import { lazy, Suspense } from 'react'
+import { Suspense } from 'react'
 import Layout from '../component/Layout.tsx'
 
-// const isProd = import.meta.env.MODE === 'production'
 
 const modules = import.meta.glob(
   [
     '../../pages/**/*.mdx',
   ], 
   { eager: true })
+
 const routes = Object.keys(modules)
-.map((filename: string) => {
-const path = filename
-  .replace (/\..\/\..\/(pages)/, '')
-  .replace(/\//g,'')
-  .replace(/\.(mdx|tsx)$/, '')
-  .replace('Index', '')
-// if(isProd) {
-//   filename = filename.replace (/\..\/\..\/(pages)/, '')
-// }
-const Component = lazy( ()=> import(/* @vite-ignore */ `${filename}`))
-return { path: `/${path}`, element: <MDXProvider><Suspense><Component /></Suspense></MDXProvider> }
-})
+  .map((filename: string) => {
+    const path = filename
+      .replace (/\..\/\..\/(pages)/, '')
+      .replace(/\//g,'')
+      .replace(/\.(mdx|tsx)$/, '')
+      .replace('Index', '')
+    //@ts-ignore
+    const Component = modules[filename].default
+    return { path: `/${path}`, element: <MDXProvider><Suspense><Component /></Suspense></MDXProvider> }
+  })
 
 const router = createBrowserRouter([
   {
