12ff1774714f5f4aaa921d14590a01a320977bc9
jayzou <chrischau666@gmail.com>
2025-04-13 19:21:48 +0800

    chore: edit text

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div className="prose m-auto">
       <h1>Jay Zou</h1>
       <p>Hey, I am Jay, the kid from 深圳, a web developer.</p>
-      <p>I code. I play. I live.</p>
+      <p>I code, I play, I live.</p>
       <p>Welcome! You can find what you want.</p>
       <p>Much ❤️</p>
     </div>
