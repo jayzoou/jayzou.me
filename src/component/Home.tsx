ac29431da60b9174254ef2e51cb70c38a5bb5724
jayzou <chrischau666@gmail.com>
2025-04-14 23:39:30 +0800

    chore: nav style

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -2,9 +2,9 @@ const Home = () => {
   return (
     <div className="prose m-auto">
       <h1>Jay Zou</h1>
-      <p>Hey, I am Jay, the kid from 深圳, a web developer.</p>
+      <p>Hi, I am Jay, the kid from 深圳, a web developer.</p>
       <p>I code, I play, I live.</p>
-      <p>Welcome! You can find what you want.</p>
+      <p>You can find what you want.</p>
       <p>Much ❤️</p>
     </div>
   )
--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -5,8 +5,11 @@ const NavBar = () => {
     <header className="flex justify-between items-center text-white">
       <Link to="/" className="ml-10 !text-white cursor-pointer transition scale-150 hover:scale-200">🤔</Link>
       <nav className='nav'>
-        <Link to="/posts">Blog</Link>
-        <a href="https://github.com/jayzoou" target='_blank'>GitHub</a>
+        <div className='spacer'></div>
+        <div className='flex gap-4 mr-10'>
+          <Link to="/posts">Blog</Link>
+          <a href="https://github.com/jayzoou" target='_blank'>GitHub</a> 
+        </div>
       </nav>
     </header>
   )
