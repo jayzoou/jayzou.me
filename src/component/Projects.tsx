2a4763931c691e0a55b59b09e7ed6bd778782ae2
jayzou <chrischau666@gmail.com>
2025-05-06 20:04:47 +0800

    chore: style

--- src/component/Projects.tsx
+++ src/component/Projects.tsx
@@ -1,12 +1,12 @@
 const Projects = () => {
   return (
-    <div className="prose m-auto">
+    <div className="m-auto">
       <h1 className="text-center">Projects</h1>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 pl-10 pr-10">
         <a href="https://github.com/jayzoou/vite-react-static" target="_blank">
-        <div className="w-60 rounded-lg p-4 my-4 bg-black:08 hover:bg-black:20 text-white">
+        <div className="w-50 rounded-lg p-4 my-4 bg-black:08 hover:bg-black:20 text-white">
           <div className="text-6">vite-react-static</div>
-          <div className="text-4">retact 18+ SSG</div>
+          <div className="text-4">react 18+ SSG</div>
         </div>
         </a>
       </div>
