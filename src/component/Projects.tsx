50bd4d44fc78d9a0b4d2b848acdcf6965a696eb2
jayzou <chrischau666@gmail.com>
2025-05-07 22:28:58 +0800

    fix: projects click

--- src/component/Projects.tsx
+++ src/component/Projects.tsx
@@ -3,8 +3,8 @@ const Projects = () => {
     <div className="m-auto">
       <h1 className="text-center">Projects</h1>
       <div className="flex flex-col gap-4 pl-10 pr-10">
-        <div className="w-50 rounded-lg p-4 my-4 bg-black:08 hover:bg-black:20 text-white">
-          <a href="https://github.com/jayzoou/vite-react-static" target="_blank">
+        <div className="w-50 pl-4 pr-4 rounded-lg my-4 bg-black:08 hover:bg-black:20 text-white">
+          <a className="block pt-4 pb-4" href="https://github.com/jayzoou/vite-react-static" target="_blank">
             <div className="text-6">vite-react-static</div>
             <div className="text-4">react 18+ SSG</div>
           </a>
