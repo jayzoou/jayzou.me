04f2ea05a133478111671fa019b0094041c4928a
jayzou <chrischau666@gmail.com>
2025-10-06 14:06:30 +0800

    fix

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar.tsx'
 
 const Layout = () => {
   let mainClass = 'px-7 py-10'
-  if (location && location.pathname.startsWith('/posts')) {
+  if (typeof location === 'object' && location.pathname.startsWith('/posts')) {
     mainClass = 'prose max-w-3xl m-auto'
   }
   return (
