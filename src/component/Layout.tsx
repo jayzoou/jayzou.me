3d03c23a94ea00a29fad41f7cff4151169038685
jayzou <chrischau666@gmail.com>
2025-11-19 19:42:53 +0800

    fix

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -1,13 +1,6 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import NavBar from './NavBar.tsx'
 
-let mainClass = 'px-7 py-10'
-if (typeof location === 'object' && location.pathname.startsWith('/posts')) {
-  mainClass = 'prose max-w-3xl m-auto'
-  console.log('Applying prose styling for posts', mainClass)
-}
-console.log('Current pathname:', mainClass)
-
 const Layout = () => {
   const location = useLocation()
   let mainClass = 'px-7 py-10'
