e91a6a2cb47f80a9f1b379bcea888c64e5806540
jayzoou <chrischau666@gmail.com>
2026-01-02 14:37:02 +0800

    feat: add nav AI assistant

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -7,6 +7,7 @@ const NavBar = () => {
         <div className='flex gap-4 mr-4'>
           <a href="/posts">Blog</a>
           <a href="/projects">Projects</a>
+          <a href="https://ai.jayzou.me/" target='_blank' rel='noopener noreferrer'>AI Assistant</a>
           <a href="https://github.com/jayzoou" target='_blank'>GitHub</a> 
         </div>
       </nav>
